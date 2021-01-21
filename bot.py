e3339057a7c9b93218c393e6112fefc6834053ab
lyihaoo <70906226+lyihaoo@users.noreply.github.com>
2021-01-21 16:24:28 +0800

    Polling

--- bot.py
+++ bot.py
@@ -328,10 +328,10 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_polling()
-    updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
+    updater.start_polling()
+    # updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
 
-    updater.bot.set_webhook('https://smutimetablebot.herokuapp.com/'+TOKEN)
+    # updater.bot.set_webhook('https://smutimetablebot.herokuapp.com/'+TOKEN)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
